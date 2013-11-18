c45d0a413a51146178bba07a50a005c01680a422
John Weachock <jweachock@gmail.com>
2013-11-17 22:01:07 -0500

    Add slide transitions

--- static/main.js
+++ static/main.js
@@ -1,29 +1,49 @@
 $(function() {
 	// Configuration
 	var config = {
-		aspect_ratio: 16/9,
 	};
 
 	// State
-	var slide = 1;
+	var slide = 0;
+	if(window.location.hash.length > 1) {
+		slide = parseInt(window.location.hash.substr(1));
+	}
 	var sections = $('section');
 	var num_slides = sections.length;
 
+	var move_slides = function(noanimate) {
+		sections.each(function(index) {
+			var opacity = 1;
+			var left = 0;
+			if(index < slide) {
+				opacity = 0;
+				left = -1 * $(window).width();
+			} else if(index > slide) {
+				opacity = 0;
+				left = $(window).width();
+			}
+			$(this).css('width', $(window).width() + 'px');
+			if(noanimate) {
+				$(this).css('left', left + 'px');
+				$(this).css('opacity', opacity);
+			} else {
+				$(this).animate({'opacity': opacity, 'left': left + 'px'}, 'slow');
+			}
+		});
+	}
+
 	// Init
-	sections.hide();
-	$('section:nth-child(' + slide + ')').show();
+	move_slides(true);
 	window.location.hash = '#' + slide;
 	$(window).click(function() {
-		$('section:nth-child(' + slide + ')').hide();
-		if(slide < num_slides) slide++;
-		$('section:nth-child(' + slide + ')').show();
+		if(slide < num_slides-1) slide++;
 		window.location.hash = '#' + slide;
+		move_slides();
 	});
 	$(window).on('contextmenu', function(e) {
 		e.preventDefault();
-		$('section:nth-child(' + slide + ')').hide();
-		if(slide > 1) slide--;
-		$('section:nth-child(' + slide + ')').show();
+		if(slide > 0) slide--;
 		window.location.hash = '#' + slide;
+		move_slides();
 	});
 });
