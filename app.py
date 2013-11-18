fd9cb07dd9553c623124c11cbf513b949ff38bff
John Weachock <jweachock@gmail.com>
2013-11-18 01:35:32 -0500

    Add Rainbow syntax highlighting and templating

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def index():
-	return open('index.html').read()
+@app.route('/<presentation>')
+def index(presentation):
+	try:
+		with open('slides/' + presentation + '.html') as content:
+			temp = {
+				'title': presentation,
+				'content': content.read(),
+				'languages': ['python', 'c'],
+			}
+			return flask.render_template('base.html', **temp)
+	except IOError:
+		return flask.abort(404)
 
 app.run(debug=True, host='0.0.0.0')
